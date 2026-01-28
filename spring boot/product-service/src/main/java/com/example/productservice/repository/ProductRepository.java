eb4ccf5893fd6ad180296c9059e1df9b099cd78e
Sanee <sanee321@example.com>
2026-01-28 08:48:14 +0530

    mandatoryfix

--- spring boot/product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++ spring boot/product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.example.productservice.repository;
 
 import com.example.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
